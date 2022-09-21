c08e2451b428d3d3c760f42a1006130ff2663609
Aviv <aviv.dror@gmail.com>
2022-09-21 11:28:04 +0200

    exercises 2 & 3

--- Lesson1/HackYourFuture.DotnetMasterclass/HackYourFuture.DotnetMasterclass.Exercises/Exercise2.cs
+++ Lesson1/HackYourFuture.DotnetMasterclass/HackYourFuture.DotnetMasterclass.Exercises/Exercise2.cs
@@ -8,7 +8,28 @@ namespace HackYourFuture.DotnetMasterclass.Exercises
 
         public static void ConvertCelsiusToFahrenheit()
         {
-            throw new NotImplementedException();
+            Console.WriteLine("this program converts degrees in celsius into farenheit");
+            Console.WriteLine("first of all, please type your local temperature in numbers");
+            var celsiusDegreesText = Console.ReadLine();
+
+            int celsiusDegrees;
+            while (!int.TryParse(celsiusDegreesText, out celsiusDegrees))
+            {
+                Console.WriteLine("please enter a valid temperature");
+                celsiusDegreesText = Console.ReadLine();
+            }
+
+            Console.WriteLine($"your local temperature is {celsiusDegreesText}°Celsius");
+
+            var farenheitDegrees = celsiusDegrees * 1.8 + 32;
+
+            Console.WriteLine("converting...");
+
+            Console.WriteLine($"your local temperature is {farenheitDegrees}°Farenheit");
+            Console.ReadLine();
+
+            //conversion formula; f = c*1.8+32
+            //throw new NotImplementedException();
         }
     }
 }
