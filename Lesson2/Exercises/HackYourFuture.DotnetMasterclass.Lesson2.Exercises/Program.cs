02b57b39093a6576af2672d98bea571723f0649d
Aviv <aviv.dror@gmail.com>
2022-09-24 02:18:29 +0200

    solution

--- Lesson2/Exercises/HackYourFuture.DotnetMasterclass.Lesson2.Exercises/Program.cs
+++ Lesson2/Exercises/HackYourFuture.DotnetMasterclass.Lesson2.Exercises/Program.cs
@@ -7,13 +7,14 @@ time.Minutes = 40;
 
 //Console.WriteLine(time.PrintTime);
 
-PrintTime(time);
+time.PrintTime();
 
-
+/*
 void PrintTime(JupiterTime time)
 {
     Console.WriteLine(time.Hours);
 }
+*/
 
 Console.ReadLine();
 
@@ -30,10 +31,9 @@ public class JupiterTime
         get;
     }
 
-    /*
+
     public void PrintTime()
     {
-        Console.WriteLine(Hours);
-    }
-    */
+        Console.WriteLine($"{Hours}:{Minutes}");
+    } 
 }
