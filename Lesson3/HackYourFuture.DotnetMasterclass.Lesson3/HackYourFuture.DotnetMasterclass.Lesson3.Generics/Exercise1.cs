42f97b2b9623f80a1622caf358195f63aecc89fc
Aviv <aviv.dror@gmail.com>
2022-09-25 13:42:06 +0200

    solution 1

--- Lesson3/HackYourFuture.DotnetMasterclass.Lesson3/HackYourFuture.DotnetMasterclass.Lesson3.Generics/Exercise1.cs
+++ Lesson3/HackYourFuture.DotnetMasterclass.Lesson3/HackYourFuture.DotnetMasterclass.Lesson3.Generics/Exercise1.cs
@@ -7,7 +7,11 @@ using System.Threading.Tasks;
 namespace HackYourFuture.DotnetMasterclass.Lesson3.Generics
 {
     // Create a class that can print the concatenated ToString() value of two generic variables
-    public class Exercise1
+    public class Exercise1<Tfirst, Tsecond>
     {
+        public void Print(Tfirst first, Tsecond second)
+        {
+            Console.WriteLine($"{first}{second}");
+        }
     }
 }
