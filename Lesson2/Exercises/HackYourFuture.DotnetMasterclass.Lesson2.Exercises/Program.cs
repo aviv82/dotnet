e69384d0ab8e686e445fd3f0cf80ffdf2f6c52d3
Aviv <aviv.dror@gmail.com>
2022-09-25 02:33:13 +0200

    solution 3

--- Lesson2/Exercises/HackYourFuture.DotnetMasterclass.Lesson2.Exercises/Program.cs
+++ Lesson2/Exercises/HackYourFuture.DotnetMasterclass.Lesson2.Exercises/Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+
+/* works!
 var time = new JupiterTime();
 time.Hours = 8;
 time.Minutes = 40;
@@ -36,5 +38,56 @@ public class JupiterTime
     {
         Console.WriteLine($"{Hours}:{Minutes}");
     }
-    */
 }
+    */
+
+var time = new JupiterTime(14, 88);
+
+time.PrintTime();
+
+Console.ReadLine();
+
+
+public class JupiterTime
+{
+    public JupiterTime(int hours, int minutes)
+    {
+        Hours = hours;
+        Minutes = minutes;
+    }
+    public int Hours {
+        get
+        {
+            return Hours;
+        }
+        set
+        {
+            if (value > 10)
+            
+                Hours = value - 10;
+            
+        }
+    }
+    public int Minutes
+    {
+        get
+        {
+            return Minutes;
+        }
+        set
+        {
+            if (value >= 60)
+
+                Minutes = value - 60;
+            //Hours = Hours + 1;
+
+        }
+    }
+
+    public void PrintTime()
+    {
+        Console.WriteLine($"{Hours}:{Minutes}");
+    }
+}
+
+
