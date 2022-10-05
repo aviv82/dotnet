c5cdfe42ab025ef455da48a3d80b98a490cd3b24
Aviv <aviv.dror@gmail.com>
2022-10-05 19:39:30 +0200

    solution 6

--- lesson 4/aspdotnet-core-demo/aspdotnet-core-demo/Controllers/UserController.cs
+++ lesson 4/aspdotnet-core-demo/aspdotnet-core-demo/Controllers/UserController.cs
@@ -13,27 +13,56 @@ namespace aspdotnet_core_demo.Controllers
     public class UserController : ControllerBase
     {
 
-     private List<User> _users = new(){
-            new User(22, "greg")
+        private List<User> _users = new(){
+            new User(22, "greg", "Bree 23", Gender.Other),
+            new User(30, "tron", "Fuggerstraat 16", Gender.Other),
+            new User(64, "lily", "Brouwersvliet 8", Gender.Female),
+            new User(47, "mei", "Cellebroederstraat 59", Gender.Other)
         };
-    [HttpGet]
-    public IActionResult GetString()
+        [HttpGet]
+        public IActionResult GetString()
         {
             return Ok("this controller is UserController");
         }
-    [HttpPost]
-    [Route("age")]
-    public IActionResult SetAge(int age)
+        [HttpPost]
+        [Route("age")]
+        public IActionResult SetAge(int age)
         {
-        if (age < 18)
-          return BadRequest("too young");
-          else return Ok("great!");
+            if (age < 18)
+                return BadRequest("too young");
+            else return Ok("great!");
         }
-    [HttpPost]
-    [Route("[action]")]
-    public IActionResult FindName([FromBody] string name)
+        [HttpPost]
+        [Route("[action]")]
+        public IActionResult FindName([FromBody] string name)
         {
             return Ok($"my name is {name}");
+        }
+        [HttpPost]
+        [Route("[action]")]
+        public IActionResult SetUser(User user)
+        {
+            return Ok($"hi, my name is {user.Name}, i am {user.Age} years old. my address is {user.Address} and i identify as {user.Gender}");
+        }
+        [HttpGet]
+        [Route("[action]")]
+        public IActionResult BrowseUserList(string name)
+        {
+            User userFound = null;
+            foreach (var user in _users)
+            {
+                if (name == user.Name) userFound = user;
+            }
+            if (userFound == null)
+                return NotFound("no such user");
+            else return Ok($"hi, my name is {userFound.Name}, i am {userFound.Age} years old. i live in {userFound.Address}, and identify as {userFound.Gender}.");
+        }
+        [HttpPost]
+        [Route("[action]")]
+        public IActionResult AddNewUser(int age, string name, string address, Gender gender)
+        {
+            _users.Add(new Models.User(age, name, address, gender));
+            return Ok(_users);
         }
    }
 }
--- lesson 4/aspdotnet-core-demo/aspdotnet-core-demo/Models/User.cs
+++ lesson 4/aspdotnet-core-demo/aspdotnet-core-demo/Models/User.cs
@@ -1,15 +1,30 @@
 ﻿using System;
+//using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
 namespace aspdotnet_core_demo.Models
 {
     public class User
     {
-        public User(int age, string name)
+        public User(int age, string name, string address, Gender gender)
         {
             Age = age;
             Name = name;
+            Address = address;
+            Gender = gender;
         }
         public int Age { get; set; }
         public string Name { get; set; }
+        public string Address { get; set; }
+        public Gender Gender { get; set; }
+    }
+
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum Gender
+    {
+        Male,
+        Female,
+        Other
     }
 }
 
