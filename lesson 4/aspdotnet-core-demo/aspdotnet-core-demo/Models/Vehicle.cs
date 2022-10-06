5f2b2da35dc5ddc6139d127ba489a34c6f959cda
Aviv <aviv.dror@gmail.com>
2022-10-07 00:44:49 +0200

    more solution 4 part 1

---
+++ lesson 4/aspdotnet-core-demo/aspdotnet-core-demo/Models/Vehicle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace aspdotnet_core_demo.Models
+{
+    public class Vehicle
+    {
+        public Vehicle(string licensePlate, string model, User? user = null)
+        {
+            LicencePlate = licensePlate;
+            Model = model;
+            Owner = user;
+        }
+        public string LicencePlate { get; set; }
+        public string Model { get; set; }
+        public User? Owner { get; set; }
+    }
+}
+
---
+++ lesson 4/aspdotnet-core-demo/aspdotnet-core-demo/dto/VehicleDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using aspdotnet_core_demo.Models;
+
+namespace aspdotnet_core_demo.dto
+{
+    public class VehicleDto
+    {
+
+        /*
+        public VehicleDto(string licencePlate, string model)
+        {
+            LicencePlate = licencePlate;
+            Model = model;
+        }
+        */
+
+        public string LicencePlate { get; set; }
+        public string Model { get; set; }
+
+        public Vehicle FromDto()
+        {
+            return new Vehicle(LicencePlate, Model);
+        }
+
+    }
+}
+
