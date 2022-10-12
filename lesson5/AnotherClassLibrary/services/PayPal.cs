b89862578bef048a9d21e05a9b9949e476201566
Aviv <aviv.dror@gmail.com>
2022-10-12 23:45:09 +0200

    lesson 5 setup

---
+++ lesson5/AnotherClassLibrary/services/PayPal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace AnotherInterfaceDemo.services
+{
+    public class PayPal
+    {
+        
+        public void HandlePayment(double price, string bankAccountNumber)
+        {
+            //handle payment
+            Console.WriteLine($"handle payment for {price}, to {bankAccountNumber}");
+        }
+    }
+}
+
---
+++ lesson5/AnotherClassLibrary/services/interfaces/IPaymentService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace AnotherInterfaceDemo.services.interfaces
+{
+    public interface IPaymentService
+    {
+        void HandlePayment(double price, string bankAccountNumber)
+        {
+            // handle payment
+            Console.WriteLine($"pay {price} to {bankAccountNumber}");
+        }
+    }
+
+}
+
